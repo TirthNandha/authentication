a9c6a6b604244d8e1db811aa17b9b43a3b96ae71
TirthNandha <ntirth15@gmail.com>
2024-04-13 12:14:55 +0530

    Added paswd encryption

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express")
 const ejs = require("ejs")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-
+const encrypt = require("mongoose-encryption")
 
 const app = express()
 
@@ -17,6 +17,9 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
+
+const secret = "Thisismynewsecret"
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']})
 
 const User = new mongoose.model("User", userSchema)
 
