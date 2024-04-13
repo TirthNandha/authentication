10e90393486c64e99bb7f77d8ad7a2fed55515b3
TirthNandha <ntirth15@gmail.com>
2024-04-13 11:40:39 +0530

    authenticated login and register page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express")
 const ejs = require("ejs")
 const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 
 
 const app = express()
@@ -9,6 +10,15 @@ const app = express()
 app.use(express.static("public"))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
+
+mongoose.connect("mongodb://0.0.0.0:27017/userDB")
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const User = new mongoose.model("User", userSchema)
 
 app.get("/", function(req, res) {
     res.render("home")
@@ -21,6 +31,30 @@ app.get("/login", function(req, res) {
 app.get("/register", function(req, res) {
     res.render("register")
 })
+
+app.post("/register", function(req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+
+    newUser.save()
+    res.render("secrets")
+})
+
+app.post("/login", function(req, res) {
+    const username = req.body.username
+    const password = req.body.password
+
+    const foundUser = User.findOne({email: username})
+
+    if(foundUser) {
+        if(foundUser.password = password) {
+            res.render("secrets")
+        }
+    }
+})
+
 
 app.listen(3000, function() {
     console.log("Server is runnig on port 3000")
